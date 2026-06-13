4b805cccd50082c7098d84cc41e7d2a10040cb9e
Alejandro Solórzano <alejandrosolorzano023@gmail.com>
2026-06-12 18:57:46 -0600

    fix: compileSdk 36 + file_picker v11 para compatibilidad Android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.juris_honoris"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
